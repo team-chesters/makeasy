ce4f4feb18808611b8da3ef2cbab0f1e4d8293bb
che5ya <che5ya@naver.com>
2024-01-03 12:16:32 +0900

    # ADD - add hasKey comment & test code

--- src/hasKey.js
+++ src/hasKey.js
@@ -1,3 +1,12 @@
+/**
+ * Checks if a nested key exists within an object.
+ * 
+ * @param {Object} obj - The object to search within.
+ * @param {string} level - The key level to traverse (nested key).
+ * @param {...string} rest - Additional key levels to traverse (nested keys).
+ * @returns {boolean} Returns true if the nested key exists, false otherwise.
+ */
+
 export default function hasKey(obj, level, ...rest) {
     if (obj === null) return false;
     if (obj === undefined) return false;
---
+++ test/hasKey.test.js
@@ -0,0 +1,14 @@
+import hasKey from '../src/hasKey';
+
+const profile = {
+    id: 1,
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'johndoe@test.com',
+    gender: 'male',
+};
+
+test('Test function "hasKey"', () => {
+    expect(hasKey(profile, 'id')).toBe(true);
+    expect(hasKey(profile, 'middle_name')).toBe(false);
+});
