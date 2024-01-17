24c3b27f45e65546f20d8a25f4fc62c025cc90de
che5ya <che5ya@naver.com>
2024-01-17 20:41:15 +0900

    # FIX - GenerateRandomBytes length bugs

--- src/GenerateRandomBytes.js
+++ src/GenerateRandomBytes.js
@@ -8,27 +8,37 @@ import ConvertDecimalToHex from "./ConvertDecimalToHex";
  * @returns {string} Returns the generated random string.
  */
 
-export default function GenerateRandomBytes(length = 20) {
-    length = Math.max(Math.floor(length), 1);
-
+export default function GenerateRandomBytes(len = 20) {
+    let length = Math.max(Math.floor(len), 1);
     const byteLength = (length % 2 === 0) ? length / 2 : (length + 1) / 2;
-    const randomBytes = new Uint8Array(byteLength);
-    
+    const defaultRandomBytes = new Uint8Array(byteLength);
+    const redefinedRandomBytes = [];
+
     if (window.crypto && window.crypto.getRandomValues) {
-        window.crypto.getRandomValues(randomBytes);
+        window.crypto.getRandomValues(defaultRandomBytes);
     } else {
         // Fallback for browsers not supporting crypto API
         return 'This browser does not support crypto API.';
     }
-    
-    let flag = 0;
-    for (const item of randomBytes) {
-        if (isEmpty(item)) flag += 1;
+
+    let flagCounter = 0;
+    for (const item of defaultRandomBytes) {
+        if (isEmpty(item)) {
+            flagCounter += 1;
+        } else {
+            redefinedRandomBytes.push(item);
+        }
     }
 
-    if (flag != 0) {
-        return 'Some bytes are empty.';
+    if (flagCounter !== 0) {
+        const backupRandomBytes = new Uint8Array(flagCounter);
+        for (let i = 0; i < flagCounter; i++) {
+            window.crypto.getRandomValues(backupRandomBytes);
+            if (!isEmpty(backupRandomBytes[i])) {
+                redefinedRandomBytes.push(backupRandomBytes[i]);
+            }
+        }
     }
 
-    return Array.from(randomBytes, ConvertDecimalToHex).join("");
+    return Array.from(redefinedRandomBytes, ConvertDecimalToHex).join("");
 };
--- test/GenerateRandomBytes.test.js
+++ test/GenerateRandomBytes.test.js
@@ -1,16 +1,16 @@
 import GenerateRandomBytes from "../src/GenerateRandomBytes";
 
 describe('GenerateRandomBytes function', () => {
-    // test('should generate random bytes with default length', () => {
-    //     const result = GenerateRandomBytes();
-    //     expect(result.length).toBe(20);
-    // });
+    test('should generate random bytes with default length', () => {
+        const result = GenerateRandomBytes();
+        expect(result.length).toBe(20);
+    });
 
-    // test('should generate random bytes with specified length', () => {
-    //     // FIXME: sometimes get random bytes in default length, 21.
-    //     const result = GenerateRandomBytes(16);
-    //     expect(result.length).toBe(16);
-    // });
+    test('should generate random bytes with specified length', () => {
+        const result = GenerateRandomBytes(34);
+        expect(result.length).toBe(34);
+    });
+
     test('should generate random bytes with default length', () => {
         const result = GenerateRandomBytes();
         expect(result.length).toBe(20); // Match the expected string length
