bbee578fc5b5dba3a239f5c3b03e810cf085e0ce
che5ya <che5ya@naver.com>
2024-02-01 22:25:14 +0900

    # EDIT - change package type - es module

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-const path = require('path')
-const { defineConfig } = require('vite')
+import { defineConfig } from 'vite';
+import path from 'path';
 
-module.exports = defineConfig({
+export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, './makeasy.js'),
+      entry: path.resolve(__dirname, './makeasy.default.js'),
       name: 'makeasy',
       fileName: (format) => `makeasy.${format}.js`
     }
+  },
+  output: {
+    exports: "named"
   }
 });
