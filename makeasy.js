eba56fb4e62ae86738a444dbc2d06aa602137ef8
che5ya <che5ya@naver.com>
2024-02-01 22:24:54 +0900

    # REMOVE - unused default index

--- makeasy.js
+++
@@ -1,8 +0,0 @@
-export { default as isArray } from './src/array/isArray.js';
-export { default as dec2Hex } from './src/convert/ConvertDecimalToHex.js';
-export { default as hsl2Hex } from './src/convert/ConvertHSLToHex.js';
-export { default as isNumber } from './src/number/isNumber.js';
-export { default as isNumberNegative } from './src/number/isNumberNegative.js';
-export { default as isEmpty } from './src/object/isEmpty.js';
-export { default as isObject } from './src/object/isObject.js';
-export { default } from './makeasy.default.js';
